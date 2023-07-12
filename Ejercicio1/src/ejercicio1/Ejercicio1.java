83e99e86a4bc234fd1b138f3d7b909435fdf770a
Danu <8391394+tonolosv@users.noreply.github.com>
2023-07-11 20:59:22 -0600

    Ejercicio1 Corriendo

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+
+public class Ejercicio1 {
+    static void pruebaConstantes()
+    {
+        final int diasLaborales = 5;
+        final int diasNoLaborales = 2;
+        System.out.println("Numero dias laborales: " + diasLaborales);
+        System.out.println("Numero dias No Laborales: " + diasNoLaborales );
+    }        
+    public static void main(String[] args) {
+        pruebaConstantes(); 
+    }
+    
+}
